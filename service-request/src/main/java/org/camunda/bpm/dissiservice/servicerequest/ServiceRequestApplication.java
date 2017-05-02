94dccac13fb0c4194ce4e600e03e2746105ea6aa
Osmar Hernandez <osmarhernandezs@gmail.com>
2017-05-01 20:19:06 -0600

    Base del proyecto final de Administracion

---
+++ service-request/src/main/java/org/camunda/bpm/dissiservice/servicerequest/ServiceRequestApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.dissiservice.servicerequest;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Service Request App")
+public class ServiceRequestApplication extends ServletProcessApplication {
+	// empty implementation
+}
